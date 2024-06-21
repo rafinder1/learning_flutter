9a9c918e10259bb0eb893038db5e41c959dc36b7
rafinder1 <rafpro33@gmail.com>
2024-06-21 13:07:05 +0200

    changed stream to bloclistener

--- 03-State-Managment-Bloc/cubit2cubit/lib/cubits/counter/counter_cubit.dart
+++ 03-State-Managment-Bloc/cubit2cubit/lib/cubits/counter/counter_cubit.dart
@@ -1,43 +1,15 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
-import 'package:cubit2cubit/cubits/color/color_cubit.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 
 part 'counter_state.dart';
 
-class CounterCubit extends Cubit<CounterState> {
-  int incrementSize = 1;
-  final ColorCubit colorCubit;
-  late final StreamSubscription colorSubscription;
+class CounterCubit extends Cubit<CounterState> {  
 
-  CounterCubit({
-    required this.colorCubit,
-  }) : super(CounterState.initial()) {
-    colorSubscription = colorCubit.stream.listen((ColorState colorState) {
-      if (colorState.color == Colors.red) {
-        incrementSize = 1;
-      } else if (colorState.color == Colors.green) {
-        incrementSize = 10;
-      } else if (colorState.color == Colors.blue) {
-        incrementSize = 100;
-      } else if (colorState.color == Colors.black) {
-        emit(state.copyWith(counter: state.counter - 100));
-        incrementSize = -100;
-      }
-    });
-  }
+  CounterCubit() : super(CounterState.initial());
 
-  void changeCounter() {
+  void changeCounter(int incrementSize) {
     emit(state.copyWith(
       counter: state.counter + incrementSize,
     ));
-  }
-
-  @override
-  Future<void> close() {
-    colorSubscription.cancel();
-    return super.close();
   }
 }
