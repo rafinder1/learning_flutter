191a3a4f4900a38335998de3345e1cd23c232e7b
rafinder1 <rafpro33@gmail.com>
2024-06-25 21:37:52 +0200

    use todoheader and createtodo in todos page

--- 03-State-Managment-Bloc/todo_app/lib/pages/todos_page.dart
+++ 03-State-Managment-Bloc/todo_app/lib/pages/todos_page.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/pages/create_todo.dart';
+
+import 'package:todo_app/pages/todo_header.dart';
 
 class TodosPage extends StatelessWidget {
   const TodosPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('TODOS'),
+    return const SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 40.0,
+            ),
+            child: Column(
+              children: [
+                TodoHeader(),
+                CreateTodo(),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
