d6c3a55e3235426212fdb11ebf8d46d3fe6073d8
rafinder1 <rafpro33@gmail.com>
2024-06-25 12:47:20 +0200

    create cubit with activate todo count cubit

---
+++ 03-State-Managment-Bloc/todo_app/lib/cubits/active_todo_count/active_todo_count_cubit.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:todo_app/cubits/todo_list/todo_list_cubit.dart';
+import 'package:todo_app/models/todo_model.dart';
+
+part 'active_todo_count_state.dart';
+
+class ActiveTodoCountCubit extends Cubit<ActiveTodoCountState> {
+  late final StreamSubscription todoListSubscription;
+  final TodoListCubit todoListCubit;
+
+  ActiveTodoCountCubit({
+    required this.todoListCubit,
+  }) : super(ActiveTodoCountState.initial()) {
+    todoListSubscription = todoListCubit.stream.listen((TodoListState todoListState) {
+      print('todoListState: $todoListState');
+
+      final int currentActivateTodoCount =
+          todoListState.todos.where((Todo todo) => !todo.completed).toList().length;
+
+      emit(state.copyWith(activeTodoCount: currentActivateTodoCount));
+    });
+  }
+
+  @override
+  Future<void> close() {
+    todoListSubscription.cancel();
+    return super.close();
+  }
+}
