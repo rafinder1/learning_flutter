a61a11122b28c180643c6abe7786e92442ff4627
rafinder1 <rafpro33@gmail.com>
2024-06-28 12:17:30 +0200

    create the todo list bloc

---
+++ 03-State-Managment-Bloc/todo_app_bloc_and_stream_subscription/lib/blocs/todo_list/todo_list_state.dart
@@ -0,0 +1,29 @@
+part of 'todo_list_bloc.dart';
+
+class TodoListState extends Equatable {
+  final List<Todo> todos;
+
+  const TodoListState({required this.todos});
+
+  factory TodoListState.initial() {
+    return TodoListState(todos: [
+      Todo(id: '1', desc: 'Clean the room'),
+      Todo(id: '2', desc: 'Wash the dish'),
+      Todo(id: '3', desc: 'Do homework')
+    ]);
+  }
+
+  TodoListState copyWith({
+    List<Todo>? todos,
+  }) {
+    return TodoListState(
+      todos: todos ?? this.todos,
+    );
+  }
+
+  @override
+  String toString() => 'TodoListState(todos: $todos)';
+  
+  @override
+  List<Object?> get props => [todos];
+}
