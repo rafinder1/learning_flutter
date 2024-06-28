691ebb77394fe4b5987ae4e792bd91a4674faff8
rafinder1 <rafpro33@gmail.com>
2024-06-28 09:53:31 +0200

    init app from previously section

---
+++ 03-State-Managment-Bloc/todo_app_bloc_and_stream_subscription/lib/cubits/active_todo_count/active_todo_count_state.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'active_todo_count_cubit.dart';
+
+class ActiveTodoCountState extends Equatable {
+  final int activeTodoCount;
+
+  const ActiveTodoCountState({required this.activeTodoCount});
+
+  factory ActiveTodoCountState.initial() {
+    return const ActiveTodoCountState(activeTodoCount: 0);
+  }
+
+  ActiveTodoCountState copyWith({
+    int? activeTodoCount,
+  }) {
+    return ActiveTodoCountState(
+      activeTodoCount: activeTodoCount ?? this.activeTodoCount,
+    );
+  }
+
+  @override
+  String toString() => 'ActiveTodoCountState(activeTodoCount: $activeTodoCount)';
+  
+  @override
+  List<Object> get props => [activeTodoCount];
+}
