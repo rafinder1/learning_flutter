2ed7e701025afe298bc7bf02f6c5826383a852c1
rafinder1 <rafpro33@gmail.com>
2024-08-17 18:13:27 +0200

    inject provider to material app

--- 04-forms-and-validation/validation_bloc/lib/src/app.dart
+++ 04-forms-and-validation/validation_bloc/lib/src/app.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:validation_bloc/src/screens/login_screen.dart';
+import 'blocs/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Log Me In',
-      home: Scaffold(body: LoginScreen(),)
+    return Provider(
+      child: const MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
+      ),
     );
   }
 }
