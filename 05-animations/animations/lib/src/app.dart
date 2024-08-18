4035b465465f5c4b090e89d6b7c3b08856d05048
rafinder1 <rafpro33@gmail.com>
2024-08-18 19:45:47 +0200

    added hom page to app

---
+++ 05-animations/animations/lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:animations/src/screens/home.dart';
+import 'package:flutter/material.dart';
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animation',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Home(),
+    );
+  }
+}
