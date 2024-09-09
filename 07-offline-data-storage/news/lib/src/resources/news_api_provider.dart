6b618991cd5c8391e19d9eb2a9312e05df4cc530
rafinder1 <rafpro33@gmail.com>
2024-09-09 21:25:06 +0200

    added type function

--- 07-offline-data-storage/news/lib/src/resources/news_api_provider.dart
+++ 07-offline-data-storage/news/lib/src/resources/news_api_provider.dart
@@ -3,21 +3,21 @@ import 'package:news/src/models/item_model.dart';
 
 import 'package:http/http.dart' as http;
 
-final _root = 'hacker-news.firebaseio.com';
+const _root = 'hacker-news.firebaseio.com';
 
 class NewsApiProvider {
   http.Client client = http.Client();
 
-  fetchTopIds() async {
+  Future<List<int>> fetchTopIds() async {
     var url = Uri.https(_root, '/v0/topstories.json');
     var response = await client.get(url);
 
     final ids = json.decode(response.body);
 
-    return ids;
+    return ids.cast<int>();
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     var url = Uri.https(_root, '/v0/item/$id.json');
     var response = await client.get(url);
 
